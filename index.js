198d5b2956e33bdf4158f33b258169d43ee9b48a
Carly <cschmitting@gmail.com>
2020-07-16 15:01:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
 }
 
 function handleClick() {
-    new Audio("sounds/tom-1.mp3").play();
+    
+    drum = this.innerHTML;
+
+    switch (drum) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play()
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play()
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play()
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play()
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play()
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play()
+            break;
+        default: console.log(drum);
+            break;
+    }
+    
 }
 
-// <img src="images/crash.png"></img>
+
+
+
+
+// new Audio("sounds/tom-1.mp3").play();
+
+// drum = document.getElementsByClassName(this.innerHTML)[0];
+//drum.style.color = "#EEE";
+
+// this.style.color = "#EEE";
